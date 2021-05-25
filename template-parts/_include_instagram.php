d054514e854def1a10fd66336dfcd59496e17320
codestickdev <35964064+codestickdev@users.noreply.github.com>
2021-05-25 17:53:44 +0200

    Update _include_instagram.php

--- template-parts/_include_instagram.php
+++ template-parts/_include_instagram.php
@@ -3,7 +3,6 @@
 		<h2 class="ssfood__secHeading" style="margin: 100px 0 50px"><?php _e('Follow us on Instagram @supersonicfood', 'codestick'); ?></h2>
 	</div>
 	<?php
-	echo do_shortcode('[elfsight_instagram_feed id="1"]');
 	if($lang == 'pl-PL'){
 		echo do_shortcode('[elfsight_instagram_feed id="1"]');
 	}elseif($lang == 'de-DE'){
