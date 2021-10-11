86d2d8ffb2198878c402fc699805eecd9d5fe0eb
codestickdev <35964064+codestickdev@users.noreply.github.com>
2021-10-11 13:09:37 +0200

    About redesign

--- page-templates/page_about.php
+++ page-templates/page_about.php
@@ -4,31 +4,110 @@
 */
 get_header(); ?>
 
-<main class="ssfood ssfood--subpage pageAbout">
-    <?php while(have_rows('about_sections')): the_row();
-        $color = get_sub_field('about_sections_color');
-        $position = get_sub_field('about_sections_position');
-        $lead = get_sub_field('about_sections_lead');
-        $title = get_sub_field('about_sections_title');
-        $content = get_sub_field('about_sections_content');
-        $image = get_sub_field('about_sections_image');
-    ?>
-    <section class="aboutSection<?php if($position == 'right'): ?> aboutSection--right<?php endif; ?>">
-        <div class="aboutSection__content" style="background-color: <?php echo $color; ?>;">
-            <p class="lead"><?php echo $lead; ?></p>
-            <h2><?php echo $title; ?></h2>
-            <div class="content"><?php echo $content; ?></div>
-        </div>
-        <div class="aboutSection__image">
-            <img src="<?php echo $image; ?>"/>
+<main class="ssfood ssfood--about">
+    <section class="aboutHeader">
+        <div class="aboutHeader__wrap container-md">
+            <h1>O SUPERSONIC Food</h1>
+            <img class="foods" src="<?php echo get_template_directory_uri() . '/images/about/aboutHeader.png'; ?>"/>
         </div>
     </section>
-    <?php endwhile; ?>
-    
-    <?php include get_template_directory() . '/template-parts/_include_instagram.php'; ?>
-
-    <?php include get_template_directory() . '/template-parts/_include_newsletter.php'; ?>
-
+    <section class="aboutContent">
+        <div class="aboutContent__wrap container-md">
+            <div class="aboutContent__text">
+                <h2>Zdrowie i efektywność osobista</h2>
+                <p class="lead">Kto chce się czuć doskonale, funkcjonować w zdrowiu i realizować cele osobiste oraz zawodowe? Wierzymy w to, że każdy ! Tylko nie każdy wie jak to osiągnąć.</p>
+                <p>W SUPERSONIC pasjonuje nas nauka i to co oferuje natura, po to aby dostarczać Wam szybkie w przygotowaniu, a jednocześnie roślinne, innowacyjne posiłki i napoje przyszłości już dzisiaj.</p>
+                <p>Jesteśmy pierwszym wyborem na świecie dla osób oczekujących od żywności oraz napojów maksymalnych efektów dla swojego zdrowia,  samopoczucia i efektywności osobistej.</p>
+            </div>
+            <div class="aboutContent__image">
+                <div class="wrap">
+                    <img src="<?php echo get_template_directory_uri() . '/images/about/content_01.png'; ?>"/>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="aboutProducts">
+        <div class="aboutProducts__heading">
+            <h2>Co oferują nasze produkty?</h2>
+        </div>
+        <div class="aboutProducts__wrap container-md">
+            <div class="aboutProducts__box">
+                <div class="icon">
+                    <img src="<?php echo get_template_directory_uri() . '/images/about/aboutIcon_01.svg'; ?>"/>
+                </div>
+                <h3>Roślinny skład</h3>
+                <p>Naturalny produkty i innowacyjne składniki</p>
+            </div>
+            <div class="aboutProducts__box">
+                <div class="icon">
+                    <img src="<?php echo get_template_directory_uri() . '/images/about/aboutIcon_02.svg'; ?>"/>
+                </div>
+                <h3>Bazujące na nauce</h3>
+                <p>Wszystko, co robimy ma poparcie w badaniach i nauce</p>
+            </div>
+            <div class="aboutProducts__box">
+                <div class="icon">
+                    <img src="<?php echo get_template_directory_uri() . '/images/about/aboutIcon_03.svg'; ?>"/>
+                </div>
+                <h3>Dobre dla zdrowia i samopoczucia</h3>
+                <p>Dbasz o odporność i dobre samopoczucie</p>
+            </div>
+            <div class="aboutProducts__box">
+                <div class="icon">
+                    <img src="<?php echo get_template_directory_uri() . '/images/about/aboutIcon_04.svg'; ?>"/>
+                </div>
+                <h3>Dobre dla efektywności osobistej</h3>
+                <p>Masz lepszy performance</p>
+            </div>
+        </div>
+        <a href="#" class="btn btn--bigFont"><span>Poznaj produkty</span></a>
+    </section>
+    <section class="aboutParts">
+        <div class="aboutParts__wrap container-md">
+            <div class="aboutParts__box">
+                <div class="content">
+                    <h2>Nauka i roślinne składniki</h2>
+                    <p class="lead">Produkty stworzone przez naukowców i dietetyków klinicznych.</p>
+                    <p>Nasi eksperci, wykorzystując wyniki badań naukowych związanych z funkcjonowaniem ludzkiego organizmu i nowoczesnego, zdrowego żywienia, stworzyli unikalną, opartą na naturalnych składnikach formułę SUPERSONIC.  Dowiedz się więcej o naszych innowacjach i składach.</p>
+                </div>
+                <img class="image" src="<?php echo get_template_directory_uri() . '/images/about/aboutParts.png'; ?>"/>
+                <img class="image image--mobile" src="<?php echo get_template_directory_uri() . '/images/about/aboutParts_mobile.png'; ?>"/>
+            </div>
+        </div>
+    </section>
+    <section class="aboutContent aboutContent--reverse">
+        <div class="aboutContent__wrap container-md">
+            <div class="aboutContent__text">
+                <h2>Zdrowie i efektywność osobista</h2>
+                <p class="lead">Kto chce się czuć doskonale, funkcjonować w zdrowiu i realizować cele osobiste oraz zawodowe? Wierzymy w to, że każdy ! Tylko nie każdy wie jak to osiągnąć.</p>
+                <p>W SUPERSONIC pasjonuje nas nauka i to co oferuje natura, po to aby dostarczać Wam szybkie w przygotowaniu, a jednocześnie roślinne, innowacyjne posiłki i napoje przyszłości już dzisiaj.</p>
+                <p>Jesteśmy pierwszym wyborem na świecie dla osób oczekujących od żywności oraz napojów maksymalnych efektów dla swojego zdrowia,  samopoczucia i efektywności osobistej.</p>
+            </div>
+            <div class="aboutContent__image aboutContent__image--map">
+                <div class="wrap">
+                    <img src="<?php echo get_template_directory_uri() . '/images/about/content_02.png'; ?>"/>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="aboutContent">
+        <div class="aboutContent__wrap container-md">
+            <div class="aboutContent__text">
+                <h2>Zdrowie i efektywność osobista</h2>
+                <p class="lead">Kto chce się czuć doskonale, funkcjonować w zdrowiu i realizować cele osobiste oraz zawodowe? Wierzymy w to, że każdy ! Tylko nie każdy wie jak to osiągnąć.</p>
+                <p>W SUPERSONIC pasjonuje nas nauka i to co oferuje natura, po to aby dostarczać Wam szybkie w przygotowaniu, a jednocześnie roślinne, innowacyjne posiłki i napoje przyszłości już dzisiaj.</p>
+                <p>Jesteśmy pierwszym wyborem na świecie dla osób oczekujących od żywności oraz napojów maksymalnych efektów dla swojego zdrowia,  samopoczucia i efektywności osobistej.</p>
+            </div>
+            <div class="aboutContent__image">
+                <div class="wrap">
+                    <img src="<?php echo get_template_directory_uri() . '/images/about/content_03.png'; ?>"/>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="aboutLead">
+        <h2>Food and Drink of the Future.<br/>Today!</h2>
+    </section>
 </main>
 
 <?php get_footer(); ?>
